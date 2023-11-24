2a9ed7dca522e87efa8fa46bf8e6a6a7aa759e00
Kun Heo <min33sky@naver.com>
2023-11-24 15:03:56 +0900

    feat: auth init

--- src/app/(marketing)/_components/footer.tsx
+++ src/app/(marketing)/_components/footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
+    <footer className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
@@ -16,6 +16,6 @@ export default function Footer() {
           </Button>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
--- src/app/(marketing)/_components/navbar.tsx
+++ src/app/(marketing)/_components/navbar.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+    <header className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
@@ -17,6 +17,6 @@ export default function Navbar() {
           </Button>
         </div>
       </div>
-    </div>
+    </header>
   );
 }

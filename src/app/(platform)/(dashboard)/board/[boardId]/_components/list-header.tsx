3a270457c8250f77eee7bc842adf384c0b659d3f
Kun Heo <min33sky@naver.com>
2023-12-29 18:19:50 +0900

    feat: card form

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -12,9 +12,10 @@ import ListOptions from './list-options';
 
 interface ListHeaderProps {
   data: List;
+  onAddCard: () => void;
 }
 
-export default function ListHeader({ data }: ListHeaderProps) {
+export default function ListHeader({ data, onAddCard }: ListHeaderProps) {
   const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -102,7 +103,7 @@ export default function ListHeader({ data }: ListHeaderProps) {
         </div>
       )}
 
-      <ListOptions data={data} onAddCard={() => {}} />
+      <ListOptions data={data} onAddCard={onAddCard} />
     </div>
   );
 }
